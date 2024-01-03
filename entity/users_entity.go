c5247f6fcdef206544d29975a102a157c1a6746e
Agus Basari <agusbasari29@gmail.com>
2024-01-03 20:45:58 +0700

    progress

--- entity/users_entity.go
+++ entity/users_entity.go
@@ -2,6 +2,20 @@ package entity
 
 import "gorm.io/gorm"
 
+type UserRole string
+
+const (
+	Admin       UserRole = "admin"
+	Creator     UserRole = "creator"
+	Participant UserRole = "participant"
+)
+
 type Users struct {
 	gorm.Model
+	ID       uint `gorm:"primaryKey;autoIncrement"`
+	Username string
+	Fullname string
+	Email    string
+	Password string
+	Role     UserRole
 }
